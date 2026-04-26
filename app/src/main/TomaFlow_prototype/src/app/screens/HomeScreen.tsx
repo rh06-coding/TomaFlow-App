3abc3de8a97fa0ea636dc415128af0ed9661d714
Nguyễn Hoàn Hải <haibrosh5c@gmail.com>
2026-04-26 11:10:40 +0700

    Delete app/src/main/TomaFlow_prototype directory

--- app/src/main/TomaFlow_prototype/src/app/screens/HomeScreen.tsx
+++
@@ -1,9 +0,0 @@
-import PomodoroTimer from "../components/PomodoroTimer";
-
-export default function HomeScreen() {
-  return (
-    <div className="h-full relative">
-      <PomodoroTimer />
-    </div>
-  );
-}
